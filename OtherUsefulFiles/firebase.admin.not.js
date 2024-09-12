a66792782a64867b4f59d5ab944c9eb9acf7faec
Chayie Lucrese <chayielucrese25@gmail.com>
2024-09-12 16:04:33 +0100

    all files

--- OtherUsefulFiles/firebase.admin.not.js
+++ OtherUsefulFiles/firebase.admin.not.js
@@ -1,24 +1,24 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../Firebase.json');
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://mobilityplanningapp.firebaseio.com'
-  });
-module.exports = {
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../Firebase.json');
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: 'https://mobilityplanningapp.firebaseio.com'
+//   });
+// module.exports = {
 
-sendDriverRequestNotification() {
-    const message = {
-      notification: {
-        title: 'New Driver Request',
-        body: 'A new driver has registered on the platform.',
-      },
-      topic: 'admin',
-    };
+// sendDriverRequestNotification() {
+//     const message = {
+//       notification: {
+//         title: 'New Driver Request',
+//         body: 'A new driver has registered on the platform.',
+//       },
+//       topic: 'admin',
+//     };
   
-    try {
-       admin.messaging().send(message);
-      console.log('Notification sent to admin');
-    } catch (error) {
-      console.error('Error sending notification:', error);
-    }
-  }}
+//     try {
+//        admin.messaging().send(message);
+//       console.log('Notification sent to admin');
+//     } catch (error) {
+//       console.error('Error sending notification:', error);
+//     }
+//   }}
